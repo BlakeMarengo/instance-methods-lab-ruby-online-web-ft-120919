b4331d5f9f623aa5fa220cc58b31ed6ee23d6c05
Blake Marengo <marengo.blake@gmail.com>
2019-12-16 21:19:45 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+  def bark
+    puts "Woof"
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+class Dog
+  # Class body
+ 
+  # Instance Method Definition
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+fido = Dog.new
+fido.bark #> "Woof!"
