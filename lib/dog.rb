d086fdced3fe43e4dffaa69da2f808b012d41b7f
Blake Marengo <marengo.blake@gmail.com>
2019-12-16 21:20:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,7 @@
 class Dog
   def bark
     puts "Woof"
+  def sit 
+    puts 'The Dog is sitting'
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-class Dog
-  # Class body
- 
-  # Instance Method Definition
-  def bark
-    puts "Woof!"
-  end
-end
- 
-fido = Dog.new
-fido.bark #> "Woof!"
